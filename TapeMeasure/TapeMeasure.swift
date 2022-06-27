9d33ecee373851cf392c4720472794054c39cf0c
Geoff Davis <geoffsdavis@yahoo.com>
2022-06-26 22:54:41 -0700

    Updated comments in TapeMeasure file

--- TapeMeasure/TapeMeasure.swift
+++ TapeMeasure/TapeMeasure.swift
@@ -2,67 +2,53 @@
 //
 //  Copyright 2022 - Geoff Davis
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+// following conditions:
 //
 //  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// ----------------------------------------------------------------------------------------------------------
+//
+//  TAPE MEASURE
+//
+// TapeMeasure is a convenience class, written in Swift, that acts as a view model for rendering a visual tape measure,
+// or any other graphical elements that need layout in a measured sequence. It can dynamically re-generate that data,
+// so that the tape measure can be scrolled and/or scaled on the fly, due to animation or user interaction.
+//
+// The class that takes a few basic parameters, and can dynamically return an array that represents the ticks of a tape measure,
+// as well as other useful information. The class itself is stateless except for those parameters, and is independent
+// of any graphics frameworks. It can be used with SwiftUI, UIKit, CoreAnimation, SpriteKit, SceneKit, or any other graphics framework
+// for MacOS or iOS.
 //
 //
+// KEY CONCEPTS
+//
+// Position - A given "physical" (graphical) position on the tape measure. For instance, on a thermometer, the position
+// for boiling water might at 300 graphical points.
+//
+// Segment - A span covering a single subdivision. On a real-world tape measure, a segment might define a distance of 1 cm, or maybe 1 inch.
+//
+// Value - The actual value of measurement at a given position. For instance, on a thermometer, the value at the position for boiling water
+// would be 100ºC (212ºF).
+//
+// Origin - The position, in graphical points, where the value is 0.0. By default, segment borders always align with this position.
+//
+// Tick - Demarcations that define segments, as well as any subdivisions of segments. Put another way, a segment can be defined
+// by one or more ticks.
+//
+// Tick Index - The index of ticks that defines a segment's starting boundary, as well as any segment subdivision boundaries.
+// An index of 0 represents the segment's own starting boundary. Indices of greater than 0 represent the segment's
+// subdivision staring boundaries.
 //
 //
-// TapeMeasure is a convenience class for creating data to render a visual tape measure, or other visual effects
-// that need to be laid out, and perhaps dynamically scrolled and/or scaled due to animation or user interaction.
-//
-//  Key Concepts:
-//
-//    * anchor value - the acutal value at a given position. For instance, on a thermometer,
-//    the value at the position for boiling water would be 100ºC (212ºF)
-//
-//    * anchor position - a given "physical" position on the tape measure. For intance, on a thermometer,
-//    the position for boiling water might at 300 pixels from the therometer's origin
-//
-//    * origin - the position at 0.0. Segment borders align with this position
-//
-//    * segment - a length which defines a single segment. On a real-world tape measure, a segment would be 1 inch
-//
-//    * tick - a subdivision of a segment. Segments can have 1 or more ticks
-//
-//    * tick index - the index of ticks within a segment (i.e. segment subdivisions). An index of 0 is at the segment's own "starting border"
-//
-//
-// TapeMeasure takes some intial parameters on instantiation, all of which can be mutated at any time.
-//
-//    The postional boundaries in which TapeMeasure Ticks will be generated. Ticks will only have positions within these boundaries.
-//    For convenience, pass in the boundaries which match the layout in the intended view. The returned Tick positions will match nicely.
-//    - positionBounds: ClosedRange<CGFloat>
-//
-//    Whether the Ticks are ascending or descending in value, within the positional boudaries
-//    - direction: Direction
-//
-//    The value per segment. This would be the value scale of the tape masure
-//    - segmentValue: Double
-//
-//    The "physical" length of a single segment. This would be the visual scale of the tape measure
-//    - segmentDistance: CGFloat
-//
-//    Pretty obvious. A value of 1 would indicated each segment is a single full tick,
-//    greater than one subdivides the segment into that many ticks
-//    - ticksPerSegment: Int
-//
-//    This optional paramenter clips a tape measure by value, *NOT* position.
-//    For instance, in a scrolling tape meausre, if you didn't want any ticks below a value of 0, but also don't want
-//    to set any upper value bound, you would pass in a closed range of 0.0...Double.greatestFiniteMagnitude
-//    - valueClippingBounds: ClosedRange<Double>? = nil
-//
-// Once the class is instantiated, when passed a given position and value on the TapeMeasure,
-// it will pass back a series of Ticks within the given positional boundaries, with all ticks positioned to scale.
-// Each Tick has a relative position within the positional boundaries, the value for that Tick, and an index.
-//
-// - A Tick index of 0 represents a segment
-// - A Tick index greater than 0 represents a subdivision of a full segment, and is useful for rendering Ticks in a view
-//      (e.g. for 4-tick segments, render text labels at index 0, long tick marks at 2, and short tick marks at 1 & 3)
-//
+
 
 import Foundation
 
@@ -73,16 +59,37 @@ public class TapeMeasure {
     
     // MARK: public parameters, exposed for mutation
     
+    // Graphical position boundaries inside which TapeMeasure.Ticks will be generated. Ticks will only have positions within these boundaries.
+    // For convenience, pass in the boundaries which match the layout in the intended view. The returned Tick positions will match nicely.
     public var positionBounds: ClosedRange<CGFloat>
+    
+    // The graphical length of a single segment, in graphical points. This would be the visual scale of the tape measure.
     public var segmentLength: CGFloat
+    
+    // The value per segment. This would be the value scale of the tape measure.
     public var segmentValue: Double
+    
+    // A value of 1 would indicated each segment is defined by a single tick, whereas greater than 1 would indicate a segment sub-divided
+    // into that many ticks.
     public var ticksPerSegment: Int
+    
+    // num describing whether the Ticks are .ascending or .descending in value, within the supplied positionBounds
     public var direction: Direction
+    
+    // This optional parameter clips a tape measure by value, NOT position. For instance, in a scrolling tape measure,
+    // if you don't want any ticks rendered below a value of 0.0, but also don't want to set any upper value bound,
+    // you would pass in a closed range of 0.0...Double.greatestFiniteMagnitude. This is nil by default, to indicate no value clipping bounds.
     public var valueClippingBounds: ClosedRange<Double>?
+    
+    // An offset by position (graphical points) of the origin (location representing the value of 0.0), from the position of 0.0 pts.
+    // This might be useful to stagger the ticks so they don't neatly line up with segments, which span a graphical distance but define units
+    // of value. We'll see an example below. This is zero by default, to indicate no offset, so that the value of 0.0 would be
+    // at the position of 0.0 pts.
     public var valueOriginOffset: Double
     
-    // Special property for dealing with precision issues
-    // when determining if ticks line up perfectly with origin
+    // Special property that provides a "fudge factor" for dealing with floating point math precision issues. TapeMeasure uses this
+    // when determining if ticks line up perfectly with origins or fall with given boundaries. Likely you won't have to change this,
+    // but if for example you're dealing with very small fractional values, change this property as needed to adjust the precision up or down.
     public var tickAlignmentEpsilon: CGFloat = 0.001
     
     
